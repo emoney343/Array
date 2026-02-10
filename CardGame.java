417a3ac901b86a677135516d80a64ed2efe75644
Eric <ericlioubarski@gmail.com>
2026-02-09 20:00:21 -0800

    Hand Elevator

--- CardGame.java
+++ CardGame.java
@@ -1,50 +1,100 @@
+import java.util.Arrays;
+
 public class CardGame {
 
-    // private static boolean isElementPresent(int[] arr, int key) {
-    //     for (int element : arr) {
-    //         if (element == key) {
-    //             return true;
-            // }
-    //     }
-    //     return false;
-    // }
+    // Method to get straight
+    static boolean isStraight(int[] arr) {
+        int[] a = arr.clone();
+        Arrays.sort(a);
 
-    // Count pairs in a hand of cards
-    private static int countPairs(Card[] hand) {
-
-        int[] counts = new int[15];
-
-        for (Card card : hand) {
-            counts[card.getValue()]++;
-
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] != a[i - 1] + 1) return false;
         }
-        
-        int pairCount = 0;
-        for (int i = 2; i < 14; i++) {
-            if (counts[i] >= 2) pairCount++;
-
-        }
-
-        return pairCount;
-
+        return true;
     }
 
-       
- 
+    // Method to get highcard
+    static int highCard(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) max = arr[i];
+        }
+        return max;
+    }
+
+    private static void evaluateAndPrint(Card[] hand) {
+
+        // Store counts
+        int[] counts = new int[15];     
+        int[] suitCounts = new int[5];
+        int[] values = new int[hand.length];
+
+        // Get values of hands and suits in hand
+        for (int i = 0; i < hand.length; i++) {
+            int v = hand[i].getValue();
+            int s = hand[i].getSuits();
+
+            values[i] = v;
+            counts[v]++;
+            suitCounts[s]++;
+        }
+
+        int pairs = 0;
+        int triples = 0;
+        int quads = 0;
+
+        for (int v = 2; v <= 14; v++) {
+            if (counts[v] == 2) pairs++;
+            else if (counts[v] == 3) triples++;
+            else if (counts[v] == 4) quads++;
+        }
+
+        boolean flush = false;
+        for (int s = 1; s <= 4; s++) {
+            if (suitCounts[s] == 5) flush = true;
+        }
+
+        boolean straight = isStraight(values);
+
+        // Print hand evaluation
+        if (flush && straight) {
+            System.out.println("Straight Flush");
+        } else if (quads == 1) {
+            System.out.println("Four of a Kind");
+        } else if (triples == 1 && pairs == 1) {
+            System.out.println("Full House");
+        } else if (flush) {
+            System.out.println("Flush");
+        } else if (straight) {
+            System.out.println("Straight");
+        } else if (triples == 1) {
+            System.out.println("Three of a Kind");
+        } else if (pairs == 2) {
+            System.out.println("Two Pair");
+        } else if (pairs == 1) {
+            System.out.println("One Pair");
+        } else {
+            System.out.println("High Card: " + highCard(values));
+        }
+    }
+
     public static void main(String[] args) {
         DeckOfCards deck = new DeckOfCards();
-
         deck.shuffle();
 
-        
+        System.out.println("Dealing 5 cards");
+        Card[] hand = deck.dealCards(5);
 
-        // Deal cards
-        System.out.println("Dealing 10 cards");
-        Card[] hand = deck.dealCards(10);
-        for (Card card : hand) System.out.println(card);
+        for (Card card : hand) {
+            System.out.println(card);
+        }
 
-        int pairCount = countPairs(hand);
-        System.out.println("Number of pairs in hand: " + pairCount);
+        evaluateAndPrint(hand);
+    }
+}
+
+
+
         
 
     
@@ -66,9 +116,9 @@ public class CardGame {
 
         // boolean res = isElementPresent(hand1, key);
         // System.out.println("Is " + key + " present in the hand: " + res);
-        }
+    
 
-    }
+    
 
 
     // SO then i need to make a if statement that checks if a certain sequence of numbers is in an array so it can add points to a total score variable and if not
